767f8f83c1e1eed45fd06324e9b2d24bdf9af93c
Joshua <simplextimor523@gmail.com>
2024-06-08 19:23:37 +0200

    sanity checks + navigation + directory

--- source/interface/user_interface.cpp
+++ source/interface/user_interface.cpp
@@ -6,7 +6,7 @@
 
 #include "user_interface.h"
 
-void UserInterface::DisplayMenu() {
+void UserInterface::DisplayMenu() { //NOLINT
     DebugPrint debug_start{};
     InputManager input_start{};
 
@@ -18,23 +18,26 @@ void UserInterface::DisplayMenu() {
     m_previous_window_rect = rect;
     m_wants_to_exit_program = false;
 
+    m_main_menu = true;
+    m_settings_menu = false;
+
     ExitNavigation();
     Navigation();
 
-    //GetUserInput();
+    debug_start.Print(m_window_padding + "Main menu", LIGHTCYAN);
 
     m_window_center = 1000;
 
     while (!m_wants_to_exit_program) {
         GetWindowRect(hwnd, &rect);
 
-        m_resize_state = false;
+        window_resize_state = false;
 
         if (rect.right - rect.left != m_previous_window_rect.right - m_previous_window_rect.left ||
             rect.bottom - rect.top != m_previous_window_rect.bottom - m_previous_window_rect.top) {
 
             m_previous_window_rect = rect;
-            m_resize_state = true;
+            window_resize_state = true;
         }
 
         m_window_width = std::abs(m_previous_window_rect.left - m_previous_window_rect.right);
@@ -52,11 +55,13 @@ void UserInterface::DisplayMenu() {
         std::string width_string = std::to_string(m_window_width);
         std::string height_string = std::to_string(m_window_height);
 
-        if (m_resize_state || input_start.IsKeyPressed(ONE)) {// window was resized
-            system("cls");   // reset everything
+        if (window_resize_state || input_start.IsKeyPressed(ONE)) {// window was resized
+            ClearScreen();  // reset everything
 
             ExitNavigation();
             Navigation();
+
+            debug_start.Print(m_window_padding + "Main menu", LIGHTCYAN);
         }
 
         if (input_start.MultipleKeyPressed(VK_CONTROL, VK_OEM_PLUS)) {
@@ -66,7 +71,70 @@ void UserInterface::DisplayMenu() {
         if (input_start.IsKeyPressed(TWO)) {
             SettingsMenu();
         }
+
+        if (input_start.IsKeyPressed(THREE) && (!m_settings_menu || !m_main_menu)) {
+            DirectoryMenu();
+        }
     }
+}
+
+void UserInterface::SettingsMenu() { //NOLINT
+    DebugPrint debug_start{};
+    InputManager input_start{};
+
+    ClearScreen();
+
+    ExitNavigation();
+    Navigation();
+
+    m_main_menu = false;
+    m_settings_menu = true;
+
+    debug_start.Print(m_window_padding + "Settings Menu", LIGHTCYAN);
+    debug_start.Blank();
+    debug_start.Print(m_window_padding + "Enter input mode? (y/n)", RED);
+    debug_start.Blank();
+
+    if (input_start.IsKeyPressed(FOUR)) {
+        m_input_mode = true;
+        debug_start.DEBUG_PRINT("Input mode enabled", GREEN);
+    } else if( input_start.IsKeyPressed(N)) {
+        m_input_mode = false;
+    }
+
+    if (m_input_mode) {
+        std::cout << m_window_padding + "Enter color: ";
+
+        std::cin.clear();
+
+        std::cin >> m_user_input;
+
+        if (std::string_view{m_user_input}.contains("blue")) {
+            debug_start.Blank();
+            debug_start.Print(m_window_padding + "This is an example.", BLUE);
+            debug_start.Blank();
+            Sleep(500);
+            ClearScreen();
+            DisplayMenu(); // return to main menu
+        } else {
+            SettingsMenu(); // restart settings menu
+        }
+    }
+}
+
+void UserInterface::DirectoryMenu() {
+    DebugPrint debug_start{};
+
+    ClearScreen();
+
+    ExitNavigation();
+    Navigation();
+
+    m_main_menu = false;
+    m_settings_menu = false;
+
+    debug_start.Print(m_window_padding + "Directory Menu", LIGHTCYAN);
+    debug_start.Blank();
 }
 
 void UserInterface::Padding() { //NOLINT
@@ -84,45 +152,13 @@ void UserInterface::Padding() { //NOLINT
     }
 }
 
-void UserInterface::SettingsMenu() { //NOLINT
-    DebugPrint debug_start{};
-    InputManager input_start{};
-
-    system("cls");
-
-    Navigation();
-
-    debug_start.Print(m_window_padding + "Settings Menu", LIGHTGREEN);
-    debug_start.Blank();
-    debug_start.Print(m_window_padding + "Select Accent Color", LIGHTGRAY);
-    debug_start.Blank();
-    debug_start.Print(m_window_padding + "This is an example.", LIGHTGRAY);
-    debug_start.Blank();
-
-    std::cout << m_window_padding + "Enter color: ";
-
-    std::cin.clear();
-
-    std::cin >> m_user_input;
-
-    if (std::string_view{m_user_input}.contains("blue")) {
-        debug_start.Blank();
-        debug_start.Print(m_window_padding + "This is an example.", BLUE);
-        debug_start.Blank();
-        Sleep(500);
-        system("cls");
-        DisplayMenu();
-    } else {
-        SettingsMenu();
-    }
-}
-
 void UserInterface::Navigation() {
     DebugPrint debug_start{};
 
     debug_start.Blank();
     debug_start.Print(m_window_padding + ASK_USER_OPTION_MAIN, DARKGRAY);
     debug_start.Print(m_window_padding + ASK_USER_OPTION_SETTINGS, DARKGRAY);
+    debug_start.Print(m_window_padding + ASK_USER_OPTION_DIRECTORY, DARKGRAY);
     debug_start.Blank();
 }
 
@@ -131,4 +167,8 @@ void UserInterface::ExitNavigation() {
 
     debug_start.Print(m_window_padding + ASK_USER_EXIT, LIGHTGRAY);
     debug_start.Print(m_window_padding + EXIT_PROGRAM_HINT, DARKGRAY);
+}
+
+void UserInterface::ClearScreen() {
+    system("cls");
 }
--- source/interface/user_interface.h
+++ source/interface/user_interface.h
@@ -1,9 +1,10 @@
 #ifndef UNTITLED_USER_INTERFACE_H
 #define UNTITLED_USER_INTERFACE_H
 
-#define ASK_USER_EXIT "     Want to exit program?"
-#define ASK_USER_OPTION_MAIN "Press 1 to enter the main menu"
-#define ASK_USER_OPTION_SETTINGS " 2 to enter the settings menu"
+#define ASK_USER_EXIT             "     Want to exit program?"
+#define ASK_USER_OPTION_MAIN      "1 to enter the main menu"
+#define ASK_USER_OPTION_SETTINGS  "2 to enter the settings menu"
+#define ASK_USER_OPTION_DIRECTORY "3 to enter directory menu"
 
 #define MINIMUM_WINDOW_WIDTH 250
 #define MINIMUM_WINDOW_HEIGHT 100
@@ -21,11 +22,14 @@ enum Padding {
 enum MenuOptions {
     menu_main = 1,
     menu_settings,
+    menu_directory
 };
 
 class UserInterface {
     public:
         void DisplayMenu();
+
+        bool window_resize_state;
 
     private:
         int m_window_width;
@@ -38,7 +42,9 @@ class UserInterface {
         std::string m_window_padding;
 
         bool m_wants_to_exit_program;
-        bool m_resize_state;
+        bool m_settings_menu;
+        bool m_main_menu;
+        bool m_input_mode;
 
         RECT m_previous_window_rect;
 
@@ -46,9 +52,13 @@ class UserInterface {
 
         void SettingsMenu();
 
+        void DirectoryMenu();
+
         void Navigation();
 
         void ExitNavigation();
+
+        void ClearScreen();
 };
 
 #endif //UNTITLED_USER_INTERFACE_H
